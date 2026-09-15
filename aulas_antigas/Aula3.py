36ab0dc980a81eb237caa4c3ae3a821c9eff1f9b
deverogerio <deverogero0001@outlook.com>
2026-09-14 22:12:41 -0300

    refactor: remover pasta de estudos da raiz do projeto

--- aulas_antigas/Aula3.py
+++
@@ -1,8 +0,0 @@
-# 1. Definindo a nossa variável
-anos_experiencia = 3
-
-# 2. O Python tomando a decisão
-if anos_experiencia >= 3:
-    print("Você já tem uma ótima bagagem técnica!")
-else:
-    print("Você está no início da sua jornada, continue firme!")
--- aulas_antigas/test_caixa.py
+++
@@ -1,8 +0,0 @@
-# Arquivo: test_caixa.py
-import pytest
-from caixa import processar_saque
-
-def test_saque_com_letras_deve_falhar():
-    # O 'with pytest.raises' é a nossa aposta de que o erro vai acontecer
-    with pytest.raises(ValueError):
-        processar_saque("ABC_FALHA_TECLADO")
